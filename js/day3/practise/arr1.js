4831496df777ae09a97b58694390cdad4ed35740
abishek17yadav <abishek17yadav@gmail.com>
2026-04-10 11:17:06 +0530

    prodCart

---
+++ js/day3/practise/arr1.js
@@ -0,0 +1,18 @@
+
+
+
+let arr= [10,9,8,1,4,6];
+// let arr1= ["z","b","a","c"];
+
+// arr1.sort();
+// console.log(arr1)
+
+
+arr.sort((a,b)=>{
+    return a-b;
+});
+console.log(arr)
+
+
+
+
---
+++ js/practise/arrays1.js
@@ -0,0 +1,18 @@
+let fruits = ["apple", "banana", "mango"];
+
+
+// fruits.shift();
+fruits.unshift("kiwi");
+fruits.push("grapes");
+
+// fruits.pop();
+// fruits.shift();
+
+console.log(fruits.length);
+
+console.log(fruits)
+
+
+// Map(value , indexe)
+// reduce(accumulator  , value) , initial value)
+
